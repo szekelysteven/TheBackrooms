001be92dd4100b218eca1a30e9da06065b57da48
Dennis Xia <dxcolony@gmail.com>
2021-11-29 15:34:08 -0500

    11/29/2021
    Added some UI elements including Parallaxing to the Background image

---
+++ Assets/UI 1/Assets/ParrallaxBackground.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParrallaxBackground : MonoBehaviour
+{
+    Vector2 pz;
+
+    Vector2 StartPosition;
+
+    [SerializeField] int moveModifier;
+
+    private void Start()
+    {
+        StartPosition = transform.position;
+
+    }
+
+    private void Update()
+    {
+        Vector2 pz = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        float posX = Mathf.Lerp(transform.position.x, StartPosition.x + (pz.x * moveModifier), 2f * Time.deltaTime);
+        float posY = Mathf.Lerp(transform.position.y, StartPosition.x + (pz.y * moveModifier), 2f * Time.deltaTime);
+
+        transform.position = new Vector3(
+            posX,
+            posY,
+            0
+            );
+    }
+
+}
