e0dad88a9761e39e1d6f8b1e32f0692261fa9e01
Dennis Xia <dxcolony@gmail.com>
2021-12-05 22:13:07 -0500

    12/5/2021
    Menu Scene Changes

--- Assets/UI 1/Assets/ParrallaxBackground.cs
+++ Assets/UI 1/Assets/ParrallaxBackground.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ParrallaxBackground : MonoBehaviour
 {
-    Vector2 pz;
 
     Vector2 StartPosition;
 
@@ -13,7 +10,6 @@ public class ParrallaxBackground : MonoBehaviour
     private void Start()
     {
         StartPosition = transform.position;
-
     }
 
     private void Update()
@@ -23,11 +19,7 @@ public class ParrallaxBackground : MonoBehaviour
         float posX = Mathf.Lerp(transform.position.x, StartPosition.x + (pz.x * moveModifier), 2f * Time.deltaTime);
         float posY = Mathf.Lerp(transform.position.y, StartPosition.x + (pz.y * moveModifier), 2f * Time.deltaTime);
 
-        transform.position = new Vector3(
-            posX,
-            posY,
-            0
-            );
+        transform.position = new Vector3(posX, posY, 0);
     }
 
 }
