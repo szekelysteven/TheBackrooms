7ddcb19cd696acc69a45b7f28ae80253cead273f
Doutigalla <gurahl249@gmail.com>
2021-12-05 19:10:01 -0500

    Door Trigger Complete

--- Assets/Albert Level/Scripts/DoorController.cs
+++ Assets/Albert Level/Scripts/DoorController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-    
+    Animator _doorAnim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _doorAnim.SetBool("isOpening", true);
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        _doorAnim.SetBool("isOpening", false);
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        _doorAnim = this.transform.parent.GetComponent < Animator>();
     }
 
     // Update is called once per frame
