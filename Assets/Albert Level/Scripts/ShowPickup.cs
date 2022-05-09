879af7d5e093c932db8aa8ff835404613f282a98
Doutigalla <gurahl249@gmail.com>
2022-05-09 12:27:30 -0400

    Character textures

---
+++ Assets/Albert Level/Scripts/ShowPickup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPickup : MonoBehaviour
+{
+    Rect rect;
+    Texture;
+        //use this for initialization
+        void Start {Rect is=new Rect( ,)
+
+            // Update is called once per frame
+    void Update()
+    {
+        float size = Screen.width * 0.1f;
+        rect = new Rext(Screen.width2 - size / 2, Screen.height * 0.7f,, size, size);
+        texture = Resources.Load  ("KeyCard/Motel3A") as Texture;
