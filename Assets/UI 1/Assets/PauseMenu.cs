fee292668054f7a0fec81d0c3c34bbc435430a29
K-Dodson <dodsonkevin777@gmail.com>
2022-05-12 16:23:25 -0400

    starting the pause menu

---
+++ Assets/UI 1/Assets/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GameIsPaused = false; /// We want it to be accesible on other scripts and we want to check to see if it is paused
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                if (GameIsPaused)
+                {
+                    Resume();
+                }else
+                {
+                    Pause();
+                }
+            }
+        }
+    }
+    void Resume () 
+    
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }    
+
+}
