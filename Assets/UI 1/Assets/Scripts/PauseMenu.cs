2753eb7f021062d06fb46ac440af66e3f4e56d4e
K-Dodson <dodsonkevin777@gmail.com>
2022-05-14 11:35:05 -0400

    pause menu

--- Assets/UI 1/Assets/Scripts/PauseMenu.cs
+++ Assets/UI 1/Assets/Scripts/PauseMenu.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static bool GameIsPaused = false; /// We want it to be accesible on other scripts and we want to check to see if it is paused
+    public static bool GameIsPaused; /// We want it to be accesible on other scripts and we want to check to see if it is paused
     public GameObject pauseMenuUI;
+
+
+    private void Start()
+    {
+        GameIsPaused = false;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (GameIsPaused)
+            
             {
                 if (GameIsPaused)
                 {
@@ -25,18 +34,33 @@ public class PauseMenu : MonoBehaviour
             }
         }
     }
-    void Resume () 
+    public void Resume () 
     
     {
+        Cursor.visible = false;
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
     void Pause()
     {
+        Cursor.visible = true;
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }    
+
+    public void LoadMenu ()
+    {
+        Debug.Log("Loading Menu");
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1f;
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
 
 }
